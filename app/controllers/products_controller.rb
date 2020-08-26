bf760d9ceef911e7373d5a91401ab1ac8eb8dcb3
Kaito Yamaki <kikngo526@gmail.com>
2020-08-26 17:03:36 +0900

    カテゴリーの設定

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,4 +2,19 @@ class ProductsController < ApplicationController
   def new
     @product =Product.new
   end
+
+  def create
+    @product = Product.new(product_params)
+    if @product.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def product_params
+    params.require(:product).permit(:name,:text,:price,:category_id,:status_id,:burden_id,:area_id,:day_id)
+  end
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,11 @@
 class Product < ApplicationRecord
+ extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :user
+  belongs_to_active_hash :category, :status, :burden, :area, :day
+
+  #空の投稿を保存できないようにする
+  validates :name, :text, :price, :category, :status, :burden, :area, :day, presence: true
+
+  #ジャンルの選択が「--」の時は保存できないようにする
+  validates :category_id, :status_id, :burden_id, :area_id, :day_id,  numericality: { other_than: 1 } 
 end
