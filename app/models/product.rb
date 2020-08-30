9f4d9ba4edcb8284aa7d4d917e1ac2d6791cbc6c
Kaito Yamaki <kikngo526@gmail.com>
2020-08-30 21:15:15 +0900

    managementテーブル作成

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,7 @@
 class Product < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :user
+  has_one :management
   belongs_to_active_hash :category
   belongs_to_active_hash :status
   belongs_to_active_hash :burden
